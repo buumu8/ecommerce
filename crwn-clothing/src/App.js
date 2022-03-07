f38248ff95e4e834bf68efc77ae90ca8ac369973
Buumu8 <buumu8@gmail.com>
2022-03-07 13:07:24 +0700

    create react app

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Homepage from './Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;

6b493d685751d6bfc7c350c7e9a22ae6235c6d43
Buumu8 <buumu8@gmail.com>
2022-03-08 15:06:02 +0700

    add header component to shoppage

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
 import Shoppage from './Pages/Shoppage/Shoppage';
+import Header from './Components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shoppage} />

4dac6d744a4447c5d4d7300d4d10af7665ab821a
Buumu8 <buumu8@gmail.com>
2022-03-09 13:24:14 +0700

    finishing signin handle submit method

--- crwn-clothing/src/Components/Signin/Signin.js
+++ crwn-clothing/src/Components/Signin/Signin.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 
 import './Signin.scss';
 
@@ -16,12 +16,19 @@ class Signin extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        })
+        const {email,password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                email: '',
+                password: ''
+            })
+        }catch(error){
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {

8bf33f6e7468d294f6012b65f242df24f055b4a8
Buumu8 <buumu8@gmail.com>
2022-03-09 13:17:59 +0700

    implement firebase utils, authen user with email/password

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,7 +4,7 @@ import Homepage from './Pages/Homepage/Homepage';
 import Shoppage from './Pages/Shoppage/Shoppage';
 import Header from './Components/Header/Header';
 import SigninAndSignuppage from './Pages/SigninAndSignuppage/SigninAndSignuppage';
-import {auth} from './firebase/firebase.util'
+import {auth,createUserProfileDocument} from './firebase/firebase.util'
 import './App.css';
 
 class App extends React.Component {
@@ -19,10 +19,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=>{
+            console.log(this.state);
+          })
+        });
+        console.log(this.state);
+      }
+      this.setState({
+        currentUser: userAuth
+      })
     })
   }
 
--- crwn-clothing/src/Pages/SigninAndSignuppage/SigninAndSignuppage.js
+++ crwn-clothing/src/Pages/SigninAndSignuppage/SigninAndSignuppage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './SigninAndSignuppage.scss';
 import Signin from '../../Components/Signin/Signin';
+import SignUp from '../../Components/SignUp/SignUp';
 
 const SigninAndSignuppage = () => (
     <div className='sign-in-and-sign-up'>
         <Signin />
+        <SignUp />
     </div>
 )
 

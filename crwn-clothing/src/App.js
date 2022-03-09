33e054983ccdf06441d0a18751b517bd83632123
Buumu8 <buumu8@gmail.com>
2022-03-09 19:07:20 +0700

    update our app to redirect to home if user signin

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 import Homepage from './Pages/Homepage/Homepage';
 import Shoppage from './Pages/Shoppage/Shoppage';
@@ -49,15 +49,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={Shoppage} />
-          <Route path='/signin' component={SigninAndSignuppage} />
+          <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to='/' />) : (<SigninAndSignuppage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
